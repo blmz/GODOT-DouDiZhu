42a689cf29264a3186ce037a0e18b70df155ff1c
北极蓝熊 <zhaojialing321@gmail.com>
2025-05-10 13:34:17 +0800

    修复手牌选择bug

--- 脚本/手牌管理器.gd
+++ 脚本/手牌管理器.gd
@@ -9,10 +9,10 @@ extends Control
 @onready var 玩家选择列表UI = $"玩家选择列表"
 
 #曲线
-@export var 选择上升曲线:Curve
-@export var 选择分散曲线:Curve
-@export_range(0, 100) var 选择上升距离:float
-@export_range(0, 100) var 选择分散距离:float
+@export var 选择上升曲线: Curve
+@export var 选择分散曲线: Curve
+@export_range(0, 100) var 选择上升距离: float
+@export_range(0, 100) var 选择分散距离: float
 
 #发牌相关
 @onready var 发牌计时器 = $"发牌计时器"
@@ -23,231 +23,250 @@ extends Control
 @onready var 下家牌数显示 = $"下家牌数显示"
 
 #扑克牌场景
-@export var 扑克牌:PackedScene
+@export var 扑克牌: PackedScene
 
 #牌堆
-var 初始牌堆:Array[扑克牌类]
+var 初始牌堆: Array[扑克牌类]
 
 #手牌列表
-var 玩家手牌列表:Array[Node] # 玩家牌UI列表
-var 上家手牌列表:Array[Node] # 上家牌UI列表
-var 下家手牌列表:Array[Node] # 下家牌UI列表
-var 玩家选择列表:Array[Node] # 玩家选择的牌列表
+var 玩家手牌列表: Array[Node] # 玩家牌UI列表
+var 上家手牌列表: Array[Node] # 上家牌UI列表
+var 下家手牌列表: Array[Node] # 下家牌UI列表
+var 玩家选择列表: Array[Node] # 玩家选择的牌列表
 
 #发牌相关
-var 是否发牌: = false
-var 发牌数: = 0
+var 是否发牌 := false
+var 发牌数 := 0
 
 #出牌相关
-var 当前选择牌:Node:
-	set(值): # 当值被改变时执行函数
-		当前选择牌 = 值
-		if (值 == null):
-			当前选择.text = "未选择"
-		else:
-			当前选择.text = 值.牌.花色 + 值.牌.点数
+var 当前选择牌: Node:
+    set(值): # 当值被改变时执行函数
+        当前选择牌 = 值
+        if (值 == null):
+            当前选择.text = "未选择"
+        else:
+            当前选择.text = 值.牌.花色 + 值.牌.点数
 
 func _process(_delta: float):
-	pass
+    pass
 
 func _input(event: InputEvent):
-	点击牌(event)
-	
+    点击牌(event)
+    
 func _ready():
-	pass
+    pass
 
 
 func 重置场景():
-	for 牌 in 玩家手牌列表:
-		牌.queue_free()
-	for 牌 in 上家手牌列表:
-		牌.queue_free()
-	for 牌 in 下家手牌列表:
-		牌.queue_free()
-	玩家手牌列表.clear()
-	上家手牌列表.clear()
-	下家手牌列表.clear()
-	初始牌堆.clear()
-	玩家选择列表.clear()
-	初始化牌堆()
-	发牌数 = 0
-	是否发牌 = false
-	发牌计时器.stop()
-	当前选择牌 = null
-	当前选择.text = "未选择"
-	玩家选择列表UI.text = "玩家选择的牌:"
+    for 牌 in 玩家手牌列表:
+        牌.queue_free()
+    for 牌 in 上家手牌列表:
+        牌.queue_free()
+    for 牌 in 下家手牌列表:
+        牌.queue_free()
+    玩家手牌列表.clear()
+    上家手牌列表.clear()
+    下家手牌列表.clear()
+    初始牌堆.clear()
+    玩家选择列表.clear()
+    初始化牌堆()
+    发牌数 = 0
+    是否发牌 = false
+    发牌计时器.stop()
+    当前选择牌 = null
+    当前选择.text = "未选择"
+    玩家选择列表UI.text = "玩家选择的牌:"
 
 #重新把牌移动到位置列表中对应的位置
 func 重置位置(位置列表:Array[Vector2]):
-	if (位置列表.size() != 玩家手牌列表.size()):
-		printerr("位置列表和手牌列表数量不对等!")
-		return 0
-	for i in range(0, 玩家手牌列表.size()):
-		玩家手牌列表[i].移动到(位置列表[i])
-		玩家手牌列表[i].旋转到(位置列表[i].angle_to_point(手牌点.position) / (2 * PI) * 360 - 90)
+    if (位置列表.size() != 玩家手牌列表.size()):
+        printerr("位置列表和手牌列表数量不对等!")
+        return 0
+    for i in range(0, 玩家手牌列表.size()):
+        玩家手牌列表[i].移动到(位置列表[i])
+        #玩家手牌列表[i].旋转到(位置列表[i].angle_to_point(手牌点.position) / (2 * PI) * 360 - 90)
+        玩家手牌列表[i].旋转到(0)
 
 #选择手牌后重新计算所有牌的位置
 func 选牌位置计算(选牌:Node) -> Array[Vector2]:
-	var 手牌数量 = 玩家手牌列表.size()
-	var 选牌编号 = 玩家手牌列表.find(选牌)
-	var 计算后位置 :Array[Vector2] = 计算位置(手牌数量)
-	var 左右最大牌数 = float(max(选牌编号, 手牌数量 - 选牌编号 - 1))
-	var 曲线段长:float = 1.0 / (左右最大牌数 * 2.0)
-	for 距中段数 in range(1, 左右最大牌数 + 1):
-		if (选牌编号 - 距中段数 >= 0):
-			手牌线点.progress_ratio = 1.0 / (手牌数量 + 1) * (选牌编号 - 距中段数 + 1)
-			手牌线点.progress -= 选择分散距离 * 选择分散曲线.sample(曲线段长 * 距中段数)
-			计算后位置[选牌编号 - 距中段数] = 手牌线点位置()
-		if (选牌编号 + 距中段数 <= 手牌数量 - 1):
-			手牌线点.progress_ratio = 1.0 / (手牌数量 + 1) * (选牌编号 + 距中段数 + 1)
-			手牌线点.progress += 选择分散距离 * 选择分散曲线.sample(曲线段长 * 距中段数)
-			计算后位置[选牌编号 + 距中段数] = 手牌线点位置()
-	return 计算后位置
+    var 手牌数量 = 玩家手牌列表.size()
+    var 选牌编号 = 玩家手牌列表.find(选牌)
+    var 计算后位置: Array[Vector2] = 计算位置(手牌数量)
+    var 左右最大牌数 = float(max(选牌编号, 手牌数量 - 选牌编号 - 1))
+    var 曲线段长: float = 1.0 / (左右最大牌数 * 2.0)
+    for 距中段数 in range(1, 左右最大牌数 + 1):
+        if (选牌编号 - 距中段数 >= 0):
+            手牌线点.progress_ratio = 1.0 / (手牌数量 + 1) * (选牌编号 - 距中段数 + 1)
+            手牌线点.progress -= 选择分散距离 * 选择分散曲线.sample(曲线段长 * 距中段数)
+            计算后位置[选牌编号 - 距中段数] = 手牌线点位置()
+        if (选牌编号 + 距中段数 <= 手牌数量 - 1):
+            手牌线点.progress_ratio = 1.0 / (手牌数量 + 1) * (选牌编号 + 距中段数 + 1)
+            手牌线点.progress += 选择分散距离 * 选择分散曲线.sample(曲线段长 * 距中段数)
+            计算后位置[选牌编号 + 距中段数] = 手牌线点位置()
+    return 计算后位置
 
 #从左到右重新排序手牌的显示z_index排序
 func 手牌显示排序():
-	var 层数 = 0;
-	for 手牌 in 玩家手牌列表:
-		手牌.z_index = 层数
-		层数 += 1
+    var 层数 = 0;
+    for 手牌 in 玩家手牌列表:
+        手牌.z_index = 层数
+        层数 += 1
 
 #按手牌数量在手牌线上计算每一个牌的位置点，使其平均分布在手牌排列线上
 func 计算位置(手牌数量:int) -> Array[Vector2]:
-	var 位置列表:Array[Vector2]=[]
-	var 分段长:float = 1.0 / (手牌数量 + 1)
-	for i in range(1, 手牌数量 + 1):
-		手牌线点.progress_ratio = 分段长 * i
-		位置列表.append(手牌线点位置())
-	手牌显示排序()
-	return 位置列表
+    var 位置列表: Array[Vector2] = []
+    var 分段长: float = 1.0 / (手牌数量 + 1)
+    for i in range(1, 手牌数量 + 1):
+        手牌线点.progress_ratio = 分段长 * i
+        位置列表.append(手牌线点位置())
+    return 位置列表
 
 #返回手牌线点的绝对位置
 func 手牌线点位置() -> Vector2:
-	var 位置:Vector2 = 手牌线点.position + 手牌线点.get_parent().position + 手牌线点.get_parent().get_parent().position
-	return 位置
+    var 位置: Vector2 = 手牌线点.position + 手牌线点.get_parent().position + 手牌线点.get_parent().get_parent().position
+    return 位置
 
 #给玩家发一张扑克牌
 func 玩家发牌(牌型:扑克牌类):
-	var 牌 = 扑克牌.instantiate()
-	add_child(牌)
-	牌.connect("鼠标进入", 鼠标进入手牌)
-	牌.connect("鼠标离开", 鼠标离开手牌)
-	牌.更新牌(牌型)
-	玩家手牌列表.append(牌)
-	
-	牌.position = 中心点.position
-	牌.rotation_degrees = 0
-	重置位置(计算位置(玩家手牌列表.size()))
+    var 牌 = 扑克牌.instantiate()
+    add_child(牌)
+    牌.connect("鼠标进入", 鼠标进入手牌)
+    牌.connect("鼠标离开", 鼠标离开手牌)
+    牌.name = "玩家手牌" + str(牌型.花色 + 牌型.点数)
+    牌.更新牌(牌型)
+    玩家手牌列表.append(牌)
+    
+    牌.position = 中心点.position
+    牌.rotation_degrees = 0
+    重置位置(计算位置(玩家手牌列表.size()))
 
 #给对家发一张扑克牌
-func 对家发牌(对家:Node,牌型:扑克牌类):
-	var 牌 = 扑克牌.instantiate()
-	对家.add_child(牌)
-	牌.更新牌(牌型)
-	牌.翻面()
-	牌.position = 中心点.position - 对家.position
-	牌.rotation_degrees = 0
-	牌.移动到(对家.position - 对家.position)
+func 对家发牌(对家:Node, 牌型:扑克牌类):
+    var 牌 = 扑克牌.instantiate()
+    对家.add_child(牌)
+    牌.更新牌(牌型)
+    牌.翻面()
+    牌.position = 中心点.position - 对家.position
+    牌.rotation_degrees = 0
+    牌.移动到(对家.position - 对家.position)
 
 #把玩家的手牌按照斗地主的牌点数花色大小从右到左排列
 func 排序手牌():
-	var 手牌复制:Array[Node]=玩家手牌列表
-	手牌复制.sort_custom(扑克牌类.比较扑克大小)	#按照扑克大小排序
-	手牌复制.reverse()	#反转列表
-	重置位置(计算位置(玩家手牌列表.size()))
-
+    # var 点数列表: Array[String] = []
+    # for i in 玩家手牌列表:
+    #     点数列表.append(i.牌.点数)
+    # print(点数列表)
+    # 点数列表.clear()
+    # for i in 玩家手牌列表:
+    #     点数列表.append(i.牌.点数)
+    # print(点数列表)
+    var 手牌复制: Array[Node] = 玩家手牌列表.duplicate()
+    手牌复制.sort_custom(扑克牌类.比较扑克大小)
+    手牌复制.reverse()
+    for i in 玩家手牌列表:
+        i.queue_free()
+    玩家手牌列表.clear()
+    for i in 手牌复制:
+        var 牌 = 扑克牌.instantiate()
+        add_child(牌)
+        牌.connect("鼠标进入", 鼠标进入手牌)
+        牌.connect("鼠标离开", 鼠标离开手牌)
+        牌.name = "玩家手牌" + str(i.牌.花色 + i.牌.点数)
+        牌.更新牌(i.牌)
+        玩家手牌列表.append(牌)
+        牌.position = i.position
+        牌.rotation_degrees = 0
+    重置位置(计算位置(玩家手牌列表.size()))
 
 #创建一副扑克牌，并打乱它
 func 初始化牌堆():
-	初始牌堆.clear()
-	初始牌堆=扑克牌类.扑克牌堆生成()
+    初始牌堆.clear()
+    初始牌堆 = 扑克牌类.扑克牌堆生成()
 
 #开始发牌
 func 开始发牌():
-	初始化牌堆()
-	牌堆牌数显示.visible = true
-	是否发牌 = true
-	发牌计时器.start(0.05)
+    初始化牌堆()
+    牌堆牌数显示.visible = true
+    是否发牌 = true
+    发牌计时器.start(0.05)
 
 #给所有玩家发牌并且留三张
 func 发牌():
-	if (初始牌堆.size() == 3):
-		是否发牌 = false
-		return 0
-	var 位置数 = 发牌数 % 3
-	match 位置数:
-		0:玩家发牌(初始牌堆.pop_front())
-		1:对家发牌(下家点,初始牌堆.pop_front())
-		2:对家发牌(上家点,初始牌堆.pop_front())
-	发牌数 += 1
-	牌堆牌数显示.更新牌数(初始牌堆.size())
-	下家牌数显示.更新牌数(下家手牌列表.size())
-	上家牌数显示.更新牌数(上家手牌列表.size())
+    if (初始牌堆.size() == 3):
+        是否发牌 = false
+        return 0
+    var 位置数 = 发牌数 % 3
+    match 位置数:
+        0: 玩家发牌(初始牌堆.pop_front())
+        1: 对家发牌(下家点, 初始牌堆.pop_front())
+        2: 对家发牌(上家点, 初始牌堆.pop_front())
+    发牌数 += 1
+    牌堆牌数显示.更新牌数(初始牌堆.size())
+    下家牌数显示.更新牌数(下家手牌列表.size())
+    上家牌数显示.更新牌数(上家手牌列表.size())
 
 func 发底牌(对象 = ""):
-	for 牌 in 初始牌堆:
-		match 对象:
-			"玩家":玩家发牌(牌)
-			"上家":对家发牌(上家点,牌)
-			"下家":对家发牌(下家点,牌)
-	初始牌堆.clear()
-	牌堆牌数显示.visible = false
-	排序手牌()
+    for 牌 in 初始牌堆:
+        match 对象:
+            "玩家": 玩家发牌(牌)
+            "上家": 对家发牌(上家点, 牌)
+            "下家": 对家发牌(下家点, 牌)
+    初始牌堆.clear()
+    牌堆牌数显示.visible = false
+    排序手牌()
 
 func 打出牌():
-	print("牌型是"+扑克牌类.牌型列表[扑克牌类.判断牌型(扑克牌类.类型转换(玩家选择列表))])
-	if (玩家选择列表.size() == 0):
-		return 0
-	for 牌 in 玩家选择列表:
-		玩家手牌列表.erase(牌)
-		牌.queue_free()
-	玩家选择列表.clear()
-	排序手牌()
+    print("牌型是" + 扑克牌类.牌型列表[扑克牌类.判断牌型(扑克牌类.类型转换(玩家选择列表))])
+    if (玩家选择列表.size() == 0):
+        return 0
+    for 牌 in 玩家选择列表:
+        玩家手牌列表.erase(牌)
+        牌.queue_free()
+    玩家选择列表.clear()
+    排序手牌()
+
 
 func 鼠标进入手牌(对象:Panel):
-	当前选择牌 = 对象
-	重置位置(选牌位置计算(对象))
+    当前选择牌 = 对象
+    重置位置(选牌位置计算(对象))
 
-func 鼠标离开手牌(_对象:Panel):
-	当前选择牌 = null
-	重置位置(计算位置(玩家手牌列表.size()))
+func 鼠标离开手牌(_对象: Panel):
+    当前选择牌 = null
+    重置位置(计算位置(玩家手牌列表.size()))
 
 func 点击牌(event):
-	#如果鼠标按下时有选中的牌，则调用选中函数
-	if (event is InputEventMouseButton):
-		if (event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
-			if (当前选择牌 != null):
-				当前选择牌.选中()
-				if (当前选择牌.是否被选中):
-					玩家选择列表.append(当前选择牌)
-				else:
-					玩家选择列表.erase(当前选择牌)
-				var 牌列表:Array[String]=[]
-				for 牌 in 玩家选择列表:
-					牌列表.append(牌.牌.花色 + 牌.牌.点数)
-				玩家选择列表UI.text = "玩家选择的牌:" + str(牌列表)
-
+    #如果鼠标按下时有选中的牌，则调用选中函数
+    if (event is InputEventMouseButton):
+        if (event.button_index == MOUSE_BUTTON_LEFT and event.pressed):
+            if (当前选择牌 != null):
+                当前选择牌.选中()
+                if (当前选择牌.是否被选中):
+                    玩家选择列表.append(当前选择牌)
+                else:
+                    玩家选择列表.erase(当前选择牌)
+                var 牌列表: Array[String] = []
+                for 牌 in 玩家选择列表:
+                    牌列表.append(牌.牌.花色 + 牌.牌.点数)
+                玩家选择列表UI.text = "玩家选择的牌:" + str(牌列表)
 
 func _on_发牌按钮_button_down():
-	开始发牌()
+    开始发牌()
 
 func _on_排序按钮_button_down():
-	排序手牌()
-	#手牌显示排序()
+    排序手牌()
 
 func _on_发牌计时器_timeout():
-	if (!是否发牌):
-		发牌计时器.stop()
-		是否发牌 = false
-		排序手牌()
-		发底牌("玩家")
-	else:
-		发牌()
+    if (!是否发牌):
+        发牌计时器.stop()
+        是否发牌 = false
+        发底牌("玩家")
+    else:
+        发牌()
 
 #位置改变时重置手牌位置
 func _on_item_rect_changed():
-	重置位置(计算位置(玩家手牌列表.size()))
+    重置位置(计算位置(玩家手牌列表.size()))
 
 func _on_重发_button_down():
-	重置场景()
-	开始发牌()
+    重置场景()
+    开始发牌()
--- 脚本/扑克牌类.gd
+++ 脚本/扑克牌类.gd
@@ -3,107 +3,130 @@ class_name 扑克牌类
 var 花色:String
 var 点数:String
 
-static  var 牌型列表=["","单张","对子","三张","三带一","三带二","顺子","连对","飞机","炸弹","王炸"]
+static  var 牌型列表=["无牌型","单张","对子","三张","三带一","三带二","顺子","连对","飞机","炸弹","王炸"]
 static  var 点数列表:Array[String] = ["3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "小王", "大王"]
 static  var 花色列表:Array[String] = ["红桃", "方片", "黑桃", "梅花"]
 
 func _init(花色参数="",点数参数=""):
-	花色=花色参数
-	点数=点数参数
+    花色=花色参数
+    点数=点数参数
 
 ##根据选择牌列表判断牌型
 static func 判断牌型(选择牌列表:Array[扑克牌类]=[])->int:
-	var 选择点数:Array[int]=[]
-	for 牌 in 选择牌列表:#获取选择牌的点数并转化为数字方便对比
-		选择点数.append(点数列表.find(牌.点数))
-	选择点数.sort()#排序
-	var 牌数:int=选择牌列表.size()
-	var 点数字典=统计(选择点数)
-	print(点数字典)
-	#判断单张
-	if 牌数==1:
-		return 1
-	#判断对子
-	if (牌数==2 and 选择牌列表[0].点数==选择牌列表[1].点数):
-		return 2
-	#判断三张
-	if (牌数==3 and 选择牌列表[0].点数==选择牌列表[1].点数 and 选择牌列表[1].点数==选择牌列表[2].点数):
-		return 3
-	#判断三带一
-	if (牌数==4 and 3 in 点数字典.values() and 1 in 点数字典.values()):
-		return 4
-	#判断三带二
-	if (牌数==4 and 3 in 点数字典.values() and 2 in 点数字典.values()):
-		return 5
-	#判断顺子
-	if (牌数>4 ):
-		var 是否是顺子:bool=false
-		for i in range(选择点数.size()-1):#通过选择点数的顺序来判断是否是顺子
-			if (选择点数[i+1]-选择点数[i]==1):
-				是否是顺子=true
-			else:
-				是否是顺子=false
-				break
-		if 是否是顺子:
-			return 6
-	
-	return 0
+    var 选择点数:Array[int]=[]
+    for 牌 in 选择牌列表:#获取选择牌的点数并转化为数字方便对比
+        选择点数.append(点数列表.find(牌.点数))
+    选择点数.sort()#排序
+    var 牌数:int=选择牌列表.size()
+    var 点数字典=统计(选择点数)
+    for i in range(点数字典.size()):
+        print(点数列表[点数字典.keys()[i]],"的数量是:",点数字典.values()[i])
+    #判断单张
+    if 牌数==1:
+        return 牌型列表.find("单张")
+    #判断对子
+    if (牌数==2 and 点数字典.size()==1):
+        return 牌型列表.find("对子")
+    #判断三张
+    if (牌数==3 and 点数字典.size()==1):
+        return 牌型列表.find("三张")
+    #判断三带一
+    if (牌数==4 and 3 in 点数字典.values() and 1 in 点数字典.values()):
+        return 牌型列表.find("三带一")
+    #判断三带二
+    if (牌数==5 and 3 in 点数字典.values() and 2 in 点数字典.values()):
+        return 牌型列表.find("三带二")
+    #判断顺子
+    if (牌数>4 and 点数字典.size()==牌数 ):
+        var 是否是顺子:bool=false
+        var 选择点数列表 = 点数字典.keys()
+        点数列表.sort()
+        for i in range(选择点数列表.size()): 
+            if i==选择点数列表.size()-1:
+                break
+            if (选择点数列表[i+1]-选择点数列表[i]==1 and 点数字典[选择点数列表[i+1]]==1):
+                是否是顺子=true
+            else:
+                return 牌型列表.find("无牌型")
+        if 是否是顺子:
+            return 牌型列表.find("顺子")
+    #判断连对
+    if (牌数>=6 and 点数字典.size()==牌数 and 牌数%2==0):
+        var 是否是连对:bool=false
+        var 选择点数列表 = 点数字典.keys()
+        点数列表.sort()
+        for i in range(选择点数列表.size()): 
+            if i==选择点数列表.size()-1:
+                break
+            if (选择点数列表[i+1]-选择点数列表[i]==1 and 点数字典[选择点数列表[i+1]]==2):
+                是否是连对=true
+            else:
+                是否是连对=false
+                break
+        if 是否是连对:
+            return 牌型列表.find("连对")
+    
+    return 牌型列表.find("无牌型")
 
-static func 排序扑克列表(选择牌列表:Array[扑克牌类]=[])->Array[扑克牌类]:
-	选择牌列表.sort_custom(比较扑克大小)
-	选择牌列表.reverse()
-	return 选择牌列表
+static func 排序扑克列表(选择牌列表: Array[扑克牌类] = []) -> Array[扑克牌类]:
+    # 直接通过比较函数控制降序，避免二次反转
+    选择牌列表.sort_custom(比较扑克大小)
+    return 选择牌列表
 
-static func 比较扑克大小(牌一,牌二)->bool:
-	var 牌1:扑克牌类
-	var 牌2:扑克牌类
-	if(牌一 is Node and 牌二 is Node):
-		牌1=牌一.牌
-		牌2=牌二.牌
-	elif(牌一 is 扑克牌类 and 牌二 is 扑克牌类):
-		牌1=牌一
-		牌2=牌二
-	else:
-		return false
-	if(牌1.点数=="大王"):
-		return false
-	if(牌2.点数=="大王"):
-		return true
-	if(牌1.点数=="小王" and 牌2.点数!="大王"):
-		return false
-	if(牌2.点数=="小王" and 牌1.点数!="大王"):
-		return true
-	if(点数列表.find(牌1.点数)>点数列表.find(牌2.点数)):
-		return false
-	if(点数列表.find(牌1.点数)<点数列表.find(牌2.点数)):
-		return true
-	if(花色列表.find(牌1.花色)>花色列表.find(牌2.花色)):
-		return false
-	return true
+static func 比较扑克大小(牌一, 牌二) -> bool:
+    # 1. 统一类型处理（支持Node和扑克牌类直接比较）
+    var 牌1: 扑克牌类
+    if (牌一 is Node):
+        牌1 = 牌一.牌 as 扑克牌类
+    else:
+        牌1 = 牌一 as 扑克牌类
+    
+    var 牌2: 扑克牌类
+    if (牌二 is Node):
+        牌2 = 牌二.牌 as 扑克牌类
+    else:
+        牌2 = 牌二 as 扑克牌类
+    if not (牌1 and 牌2):
+        return false
+    # 2. 特殊牌优先级判断（大王/小王）
+    match [牌1.点数, 牌2.点数]:
+        ["大王", _]: return false  # 牌1是大王时永远最大
+        [_, "大王"]: return true   # 牌2是大王时牌1较小
+        ["小王", _]: return false if 牌2.点数 != "大王" else true
+        [_, "小王"]: return true if 牌1.点数 != "大王" else false
+
+    # 3. 点数比较（缓存查找结果避免重复计算）
+    var 点数1 = 点数列表.find(牌1.点数)
+    var 点数2 = 点数列表.find(牌2.点数)
+    if 点数1 != 点数2:
+        return 点数1 < 点数2  # 降序排列
+
+    # 4. 花色比较
+    return 花色列表.find(牌1.花色) < 花色列表.find(牌2.花色)
 
 ##生成一个扑克牌牌堆,是否打乱:是否将扑克牌打乱，默认是true。结果将会返回一个扑克牌牌堆
 static func 扑克牌堆生成(是否打乱:bool=true)->Array[扑克牌类]:
-	var 牌堆:Array[扑克牌类]=[]
-	for 花色_ in 花色列表:#添加扑克牌
-		for i in range(13):
-			牌堆.append(扑克牌类.new(花色_, 点数列表[i]))
-	牌堆.append(扑克牌类.new("", "大王"))#添加大小王
-	牌堆.append(扑克牌类.new("", "小王"))
-	if 是否打乱:
-		牌堆.shuffle() # 打乱牌堆
-	return 牌堆
+    var 牌堆:Array[扑克牌类]=[]
+    for 花色_ in 花色列表:#添加扑克牌
+        for i in range(13):
+            牌堆.append(扑克牌类.new(花色_, 点数列表[i]))
+    牌堆.append(扑克牌类.new("", "大王"))#添加大小王
+    牌堆.append(扑克牌类.new("", "小王"))
+    if 是否打乱:
+        牌堆.shuffle() # 打乱牌堆
+    return 牌堆
 
 static func 类型转换(输入列表:Array[Node])->Array[扑克牌类]:
-	var 输出列表:Array[扑克牌类]=[]
-	for 牌 in 输入列表:
-		输出列表.append(牌.牌)
-	return 输出列表
+    var 输出列表:Array[扑克牌类]=[]
+    for 牌 in 输入列表:
+        输出列表.append(牌.牌)
+    return 输出列表
 
 static func 统计(输入数组:Array)-> Dictionary:
-	var 统计字典:Dictionary={}
-	for 元素 in 输入数组:
-		if 统计字典.has(元素):
-			统计字典[元素]+=1
-		else:
-			统计字典[元素]=1
-	return 统计字典
+    var 统计字典:Dictionary={}
+    for 元素 in 输入数组:
+        if 统计字典.has(元素):
+            统计字典[元素]+=1
+        else:
+            统计字典[元素]=1
+    return 统计字典
